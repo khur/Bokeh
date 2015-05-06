184fbf6155de499861644772006d4e98a823c853
Richard <rluo0506@gmail.com>
2015-05-05 21:18:15 -0400

    housekeeping

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :user do
     image "MyString"
-name "MyString"
-email "MyString"
-username "MyString"
-password_digest "MyString"
+    name "MyString"
+    email "MyString"
+    username "MyString"
+    password_digest "MyString"
   end
 
 end
