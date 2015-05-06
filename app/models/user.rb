49021a268e88cf3ac02537371c444e8772cfd1bd
Richard <rluo0506@gmail.com>
2015-05-06 18:27:56 -0400

    built some user TDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_many :photos
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { in: 3..16 }
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  # validates :password, presence: true, confirmation: true, length: { in: 6..20 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "is invalid without a name" do
+    user = FactoryGirl.build_stubbed(:user, name: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a username" do
+    user = FactoryGirl.build_stubbed(:user, username: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid if username is not unique" do
+    user = FactoryGirl.create(:user, email: "Richard@example.com", username: "Richard")
+    user2 = FactoryGirl.build_stubbed(:user, username: "Richard")
+    expect(user2).to be_invalid
+  end
+
+  it "is invalid if username is not between 3-16 characters" do
+    user = FactoryGirl.build_stubbed(:user, username: "12")
+    user2 = FactoryGirl.build_stubbed(:user, email: "dug@example.net", username: "12345678901234567")
+    expect(user).to be_invalid
+    expect(user2).to be_invalid
+  end
+
+  it "is invalid if username is in the wrong format"
+
+  it "is invalid without an email" do
+    user = FactoryGirl.build_stubbed(:user, email: nil)
+    expect(user).to be_invalid
+  end
+
+  it "is invalid if email is not unique" do
+    user = FactoryGirl.create(:user, username: "user1", email: "Richard@example.com")
+    user2 = FactoryGirl.build_stubbed(:user, username: "user2", email: "Richard@example.com")
+    expect(user2).to be_invalid
+  end
+
+  it "is invalid if email is in the wrong format" do
+    user = FactoryGirl.build_stubbed(:user, email: "Richard@example")
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a password"
+
+  it "is invalid unless password is 6-20 characters"
+
+  it "is invalid without correct password confirmation" 
 end
