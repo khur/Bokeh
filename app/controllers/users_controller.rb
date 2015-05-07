bf0a6cc76af9d2e98d8a062a99a7ad21dd8be03e
Richard <rluo0506@gmail.com>
2015-05-07 18:22:59 -0400

    login/signup works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id.to_s
+      flash[:welcome] = "Thanks for signing up, #{@user.username}!"
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +28,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
