6be83b4634a7b20b5d10fd053d73103e54ffd31d
Richard <rluo0506@gmail.com>
2015-05-16 23:46:59 -0400

    fix carrierwave.rb typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:      ENV['S3_KEY'],     # required
-    aws_secret_access_key:  ENV['S3_SECRET'],  # required
-    region:                 ENV['S3_REGION'],
-  }
-  config.fog_directory: =  ENV['S3_BUCKET']     # required
 
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',            # required
+    :aws_access_key_id      => ENV['S3_KEY'],    # required
+    :aws_secret_access_key  => ENV['S3_SECRET'], # required
+    :region                 => ENV['S3_REGION'], # optional, defaults to 'us-east-1'
+  }
+
+  config.fog_directory   = ENV['S3_BUCKET']     # required
 end
